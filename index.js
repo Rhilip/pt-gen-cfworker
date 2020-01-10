998715e7fb6f8d03764a5c04c94ade855707db7f
Rhilip <Rhilip@users.noreply.github.com>
2020-01-11 00:52:16 +0800

    debug: Add `&debug=1` params
    debug function from https://github.com/bustle/cf-sentry/blob/master/sentry.js

--- index.js
+++ index.js
@@ -128,9 +128,15 @@ async function handle(event) {
       // 添加缓存，此处如果response如果为undefined的话会抛出错误
       event.waitUntil(cache.put(request, response.clone()));
     } catch (e) {
-      response = makeJsonResponse({
+      let err_return = {
         error: `Internal Error, Please contact @${AUTHOR}. Exception: ${e.message}`
-      });
+      };
+      
+      if (uri.searchParams.get("debug") == '1') {
+        err_return['debug'] = debug_get_err(e, request);
+      }
+
+      response = makeJsonResponse(err_return);
       // 当发生Internal Error的时候不应该进行cache
     }
   }
@@ -208,6 +214,66 @@ function getDoubanApiKey() {
 
 function getNumberFromString(raw) {
   return (raw.match(/[\d,]+/) || [0])[0].replace(/,/g, "");
+}
+
+function debug_get_err(err, request) {
+  const errType = err.name || (err.contructor || {}).name
+  const frames = parse_err(err)
+  const extraKeys = Object.keys(err).filter(key => !['name', 'message', 'stack'].includes(key))
+  return {
+    message: errType + ': ' + (err.message || '<no message>'),
+    exception: {
+      values: [
+        {
+          type: errType,
+          value: err.message,
+          stacktrace: frames.length ? { frames: frames.reverse() } : undefined,
+        },
+      ],
+    },
+    extra: extraKeys.length
+      ? {
+          [errType]: extraKeys.reduce((obj, key) => ({ ...obj, [key]: err[key] }), {}),
+        }
+      : undefined,
+    timestamp: Date.now() / 1000,
+    request:
+      request && request.url
+        ? {
+            method: request.method,
+            url: request.url,
+            query_string: request.query,
+            headers: request.headers,
+            data: request.body,
+          }
+        : undefined,
+  }
+}
+
+function parse_err(err) {
+  return (err.stack || '')
+    .split('\n')
+    .slice(1)
+    .map(line => {
+      if (line.match(/^\s*[-]{4,}$/)) {
+        return { filename: line }
+      }
+
+      // From https://github.com/felixge/node-stack-trace/blob/1ec9ba43eece124526c273c917104b4226898932/lib/stack-trace.js#L42
+      const lineMatch = line.match(/at (?:(.+)\s+\()?(?:(.+?):(\d+)(?::(\d+))?|([^)]+))\)?/)
+      if (!lineMatch) {
+        return
+      }
+
+      return {
+        function: lineMatch[1] || undefined,
+        filename: lineMatch[2] || undefined,
+        lineno: +lineMatch[3] || undefined,
+        colno: +lineMatch[4] || undefined,
+        in_app: lineMatch[5] !== 'native' || undefined,
+      }
+    })
+    .filter(Boolean)
 }
 
 // 各个资源站点的相应请求整理方法，统一使用async function
